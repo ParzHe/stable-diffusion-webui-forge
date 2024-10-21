0f6bb29a636a1e8ce0a1ee8fd97ebfcb19590db7
Lintao <xinhuawinshare@qq.com>
2024-10-21 18:08:56 +0800

    Adjustments

--- webui-user.sh
+++ webui-user.sh
@@ -7,8 +7,9 @@
 models_dir="$HOME/fssd/models"
 clip_models_path="$models_dir/CLIP"
 vae_dir="$models_dir/VAE"
+text_encoder_dir="$models_dir/text_encoder"
 
-export GRADIO_TEMP_DIR="$HOME/fssd/tmp/"
+export GRADIO_TEMP_DIR="$HOME/fssd/tmp"
 export HUGGINGFACE_HUB_CACHE="$HOME/fssd/HF_cache"
 export HF_HOME="$HOME/fssd/HF"
 export HF_HUB_ENABLE_HF_TRANSFER=1
@@ -16,11 +17,38 @@ export HF_HUB_ENABLE_HF_TRANSFER=1
 # Copy files and models from folder of the scipts to the fssd for Casdao platform
 if [ ! -d $models_dir ]
 then
-    echo "################################################################"
-    echo "Copy models folder from $HOME/stable-diffusion-webui to fssd ..."
-    time cp -r -n $HOME/stable-diffusion-webui/models $HOME/fssd
-    echo "################################################################"
+    mkdir -p $HOME/fssd/models
 fi
+
+echo "################################################################"
+echo "Copy models folder from $HOME/stable-diffusion-webui to fssd ..."
+time cp -r -n $HOME/stable-diffusion-webui/models $HOME/fssd
+echo "################################################################"
+echo "Copy text encoder models for flux from fshare/models/comfyanonymous/flux_text_encoders to $text_encoder_dir"
+time cp -n -v $HOME/fshare/models/comfyanonymous/flux_text_encoders/t5xxl_fp16.safetensors $text_encoder_dir
+
+echo "################################################################"
+echo "Copy Flux-dev to $models_dir/Stable-diffusion"
+total_memory=$(nvidia-smi --query-gpu=memory.total --format=csv,noheader,nounits)
+if [ $total_memory -gt 25000 ]
+then
+    time cp -n -v $HOME/fshare/models/black-forest-labs/FLUX.1-dev/flux1-dev.safetensors $models_dir/Stable-diffusion
+else
+    time cp -n -v $HOME/fshare/models/Comfy-Org/flux1-dev/flux1-dev-fp8.safetensors $models_dir/Stable-diffusion
+fi
+
+echo "################################################################"
+echo "Copy Flux-dev LoRAs to $models_dir/Lora"
+x_flux_lora="$HOME/fshare/models/XLabs-AI/flux-lora-collection"
+cp -n -v $x_flux_lora/anime_lora.safetensors $models_dir/Lora
+cp -n -v $x_flux_lora/art_lora.safetensors $models_dir/Lora
+cp -n -v $x_flux_lora/disney_lora.safetensors $models_dir/Lora
+cp -n -v $x_flux_lora/furry_lora.safetensors $models_dir/Lora
+cp -n -v $x_flux_lora/mjv6_lora.safetensors $models_dir/Lora
+cp -n -v $x_flux_lora/realism_lora.safetensors $models_dir/Lora
+cp -n -v $x_flux_lora/scenery_lora.safetensors $models_dir/Lora
+
+echo "################################################################"
 
 # Install directory without trailing slash
 #install_dir="/home/$(whoami)"
@@ -29,7 +57,7 @@ fi
 #clone_dir="stable-diffusion-webui"
 
 # Commandline arguments for webui.py, for example: export COMMANDLINE_ARGS="--medvram --opt-split-attention"
-export COMMANDLINE_ARGS="--listen --skip-prepare-environment --xformers --models-dir $models_dir --clip-models-path $clip_models_path --vae-dir $vae_dir --enable-insecure-extension-access"
+export COMMANDLINE_ARGS="--cuda-malloc --listen --xformers --skip-python-version-check --skip-torch-cuda-test --skip-prepare-environment --models-dir $models_dir --clip-models-path $clip_models_path --text-encoder-dir $text_encoder_dir --vae-dir $vae_dir --enable-insecure-extension-access"
 
 # python3 executable
 #python_cmd="python3"
@@ -62,6 +90,6 @@ export LAUNCH_SCRIPT="launch.py"
 export ACCELERATE="True"
 
 # Uncomment to disable TCMalloc
-#export NO_TCMALLOC="True"
+export NO_TCMALLOC="True"
 
 ###########################################
